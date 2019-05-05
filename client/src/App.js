055ec1dfa73645b1d805c1099856f46fffde47d5
Monty <tuckerjsarah@gmail.com>
2019-05-05 18:31:20 -0400

    g

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import homeRoute from './components/pages/routes/homeRoute';
-import Library from './components/pages/library';
+import Search from './components/Search/search';
+import Library from './components/Library/library';
 import Navbar from './components/Navbar/navbar';
 import { BrowserRouter, Route } from "react-router-dom";
 import Background from './images/white-waves.png'
@@ -13,8 +13,8 @@ function App() {
     <BrowserRouter>
     <div className="App" style= {myBackgroung}>
       <Navbar/>
-      <Route exact path = '/' component= {homeRoute}/>
-      <Route exact path = '/search' component= {homeRoute}/>
+      <Route exact path = '/' component= {Search}/>
+      <Route exact path = '/search' component= {Search}/>
       <Route exact path = '/library' compenent= {Library}/>
     </div>
     </BrowserRouter>
--- client/src/components/Library/library.js
+++ client/src/components/Library/library.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Axios from 'axios';
-import './search.css';
+import '../Search/search.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
