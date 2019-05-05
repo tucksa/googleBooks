8648b8d0063e755367314a951faede1f9cdbad50
Monty <tuckerjsarah@gmail.com>
2019-05-05 18:03:32 -0400

    d

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import homeRoute from './components/pages/routes/homeRoute';
+import libraryRoute from './components/pages/routes/libraryRoute';
 import Navbar from './components/Navbar/navbar';
 import { BrowserRouter, Route } from "react-router-dom";
-import libraryRoute from './components/pages/routes/libraryRoute';
 import Background from './images/white-waves.png'
 const myBackgroung = {
   backgroundImage: "url("+Background+")",
@@ -13,9 +13,9 @@ function App() {
     <BrowserRouter>
     <div className="App" style= {myBackgroung}>
       <Navbar/>
-      <Route path = '/library' compenent= {libraryRoute}/>
       <Route exact path = '/' component= {homeRoute}/>
       <Route exact path = '/search' component= {homeRoute}/>
+      <Route exact path = '/library' compenent= {libraryRoute}/>
     </div>
     </BrowserRouter>
 
